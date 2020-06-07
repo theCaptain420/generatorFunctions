79803001e570b51bce07bca42e2c6373d7167944
thecaptain420 <mads.nielsen@hotmail.com>
2020-06-07 19:42:57 +0200

    breakpoint

--- fetchingInterTwice.js
+++ fetchingInterTwice.js
@@ -1,19 +1,20 @@
 const fetch = require("node-fetch")
 
 
-function requestStuff(url) { //simple fetch
+function requestStuff(url) { ///simple request
     return fetch(url).then(respJson => respJson.json()).catch(err => console.log(err));
 }
 
-
- function* getFirstComment() {
+function* getFirstComment() {
     try {
 
-        var result1 = yield  requestStuff("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
-        var resID =  result1.kids[0];
+        //gets the story and the comments id(kids of type array)
+        var result1 = yield requestStuff("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
+        var resID = result1.kids[0];
 
-        var result2 = yield  requestStuff("https://hacker-news.firebaseio.com/v0/item/" + resID + ".json?print=pretty")
-        var resText =  result2.text
+        //gets the comment text given the 
+        var result2 = yield requestStuff("https://hacker-news.firebaseio.com/v0/item/" + resID + ".json?print=pretty")
+        var resText = result2.text
 
         console.log(resText.text)
 
@@ -24,15 +25,14 @@ function requestStuff(url) { //simple fetch
 
 let main = getFirstComment()
 
-
-async function runThroughIt(generator){
+async function runThroughIt(generator) {
     let stepOne = await generator.next()
     console.log(stepOne)
     let stepTwo = await generator.next()
     console.log(stepTwo)
     let stepThree = await generator.next()
     console.log(stepThree)
-    
+
 }
 
 runThroughIt(main)
