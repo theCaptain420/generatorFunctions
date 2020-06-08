d0de4d19242222646459144d823e2ec07a888740
thecaptain420 <mads.nielsen@hotmail.com>
2020-06-08 16:51:52 +0200

    fetching now works

--- fetchingInterTwice.js
+++ fetchingInterTwice.js
@@ -1,22 +1,34 @@
 const fetch = require("node-fetch")
 
 
-function requestStuff(url) { ///simple request
-    return fetch(url).then(respJson => respJson.json()).catch(err => console.log(err));
+async function requestStuff(url) { ///simple request
+    const res = await fetch(url)
+        .then(respJson => respJson.json())
+        .catch(err => console.log(err));
+
+    return res;
+
 }
 
-function* getFirstComment() {
+async function* getFirstComment() {
     try {
 
         //gets the story and the comments id(kids of type array)
-        var result1 = yield requestStuff("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
+        var result1 = await requestStuff("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty");
+
+        //gotta yield the result.
+        //when running .next(), it runs the code down to the "yield" youre running
+        yield result1;
+
         var resID = result1.kids[0];
 
-        //gets the comment text given the 
-        var result2 = yield requestStuff("https://hacker-news.firebaseio.com/v0/item/" + resID + ".json?print=pretty")
+
+        //gets the comment-text given the comment-id
+        var result2 = await requestStuff("https://hacker-news.firebaseio.com/v0/item/" + resID + ".json?print=pretty")
         var resText = result2.text
 
-        console.log(resText.text)
+
+        yield resText;
 
     } catch (error) {
         console.log(error)
@@ -26,12 +38,15 @@ function* getFirstComment() {
 let main = getFirstComment()
 
 async function runThroughIt(generator) {
-    let stepOne = await generator.next()
-    console.log(stepOne)
+    //prints the value from the first fetch.
+    let stepOne = await generator.next();
+    console.log("res from step 1! ", stepOne);
     let stepTwo = await generator.next()
-    console.log(stepTwo)
+    console.log("res from step 2: ", stepTwo);
+
+    //will return undefined/done:true because the function has finished evaluating!
     let stepThree = await generator.next()
-    console.log(stepThree)
+    console.log("res from step 3:",stepThree)
 
 }
 
